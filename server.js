8d4603265885d7f3120d6f5a7282fe2a504e2ab8
matheuspavin <pavinmatheus@gmail.com>
2018-05-04 20:24:50 -0300

    Starting server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+  app = express(),
+	port = process.env.PORT || 4300;
+const bodyParser = require('body-parser');
+
+const clientsRoute = require('./server/routes/clientsRoute')
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use('/client', express.static('client/'));
+
+app.all('*', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+	next();
+});
+
+app.get("/", function (req, res) {
+    res.redirect("/client");
+});
+
+app.use('/clients', clientsRoute);
+
+const errorHandler = function (err, req, res, next) {
+	console.error('Error:', err);
+	res.status(500).send({ message: "server error" });
+};
+
+app.options('*', function (req, res, next) {
+    res.end();
+});
+
+app.use(errorHandler);
+app.listen(port);
+
+console.log('API started on: ' + port);
